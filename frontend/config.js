5f8841aaeffe49c08cfb081baab1fe65137b9ff3
Jim_laptop <jim@example.com>
2026-01-08 19:11:35 +0800

    修改無法下載問題, dockerignore修正

--- frontend/config.js
+++ frontend/config.js
@@ -12,7 +12,7 @@ window.API_CONFIG = {
     independentTtest: "/ttest/independentTtest/upload",
     pairedTtest: "/ttest/pairedTtest/upload",
     anova: "/anova/anova/upload",
-    download: "/stat/download",
+    download: "/stat/download/",
     downloadZip: "/stat/download_zip"
   }
 };
